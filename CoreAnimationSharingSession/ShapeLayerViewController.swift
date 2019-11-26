d6327a706e472471afe3254a1963e081448ca079
Ambar Septian <ambar.septian@tokopedia.com>
2019-11-27 00:24:52 +0700

    add shapes

--- CoreAnimationSharingSession/ShapeLayerViewController.swift
+++ CoreAnimationSharingSession/ShapeLayerViewController.swift
@@ -9,23 +9,83 @@
 import UIKit
 
 class ShapelayerViewController: UIViewController {
-
+    
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.contentSize = CGSize(width: view.bounds.width, height: 3000)
+        return scrollView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.layer.addSublayer(shapeLayer)
+        view.addSubview(scrollView)
+        
+        let shapeLayer = createShapeLayer(shape: .circle, position: 0)
+        scrollView.layer.addSublayer(shapeLayer)
+        
+        let starLayer = createShapeLayer(shape: .star, position: 1)
+        scrollView.layer.addSublayer(starLayer)
+        
+        let triangeLayer = createShapeLayer(shape: .triangle, position: 2)
+        scrollView.layer.addSublayer(triangeLayer)
+        
+//        strokeAndFillLayer.strokeColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+//        strokeAndFillLayer.backgroundColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
+        
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
     }
     
-    let shapeLayer: CAShapeLayer = {
+    
+    func createShapeLayer(shape: Shape, position: Int) -> CAShapeLayer {
         let layer = CAShapeLayer()
-        let frame = CGRect(origin: CGPoint(x: 20, y: 100), size: CGSize(width: 200, height: 200))
+        let spacing: CGFloat = 20
+        let size: CGFloat = 200
+        let yOrigin = (CGFloat(position) * size) + (spacing * CGFloat(position + 1))
+        let frame = CGRect(origin: CGPoint(x: 30, y: yOrigin),
+                           size: CGSize(width: size, height: size))
         layer.frame = frame
-        let bounds = layer.bounds
         
+        layer.path = shape.renderPath(layer).cgPath
+        layer.fillColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+        layer.strokeColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+        layer.lineWidth = 4
+//        layer.borderWidth = 5
+        
+        return layer
+    }
+    
+}
+
+
+
+enum Shape {
+    case circle
+    case triangle
+    case star
+    
+    func renderPath(_ layer: CALayer) -> UIBezierPath {
+        switch self {
+        case .circle:
+            return renderCirclePath(layer)
+        case .triangle:
+            return renderTrianglePath(layer)
+        case .star:
+            return renderStarPath(layer)
+        }
+    }
+    
+    private func renderCirclePath(_ layer: CALayer) -> UIBezierPath {
+        let bounds = layer.bounds
+        return UIBezierPath(ovalIn: bounds)
+    }
+    
+    private func renderTrianglePath(_ layer: CALayer) -> UIBezierPath {
+        let bounds = layer.bounds
         let trianglePath = UIBezierPath()
         let topCenterPosition = CGPoint(x: bounds.midX, y: bounds.minY)
         trianglePath.move(to: topCenterPosition)
@@ -36,14 +96,56 @@ class ShapelayerViewController: UIViewController {
         trianglePath.addLine(to: bottomRightPosition)
         trianglePath.close()
         
-        layer.path = trianglePath.cgPath
-        layer.fillColor = UIColor.blue.cgColor
-        layer.strokeColor = UIColor.black.cgColor
-        layer.lineWidth = 10
-        layer.backgroundColor = UIColor.gray.cgColor
+        return trianglePath
+    }
+    
+    private func renderStarPath(_ layer: CALayer) -> UIBezierPath {
+        let starPath = UIBezierPath()
+        let center = CGPoint(x: layer.bounds.midX, y: layer.bounds.midY)
+      
+        let numberOfPoints = CGFloat(5.0)
+        let numberOfLineSegments = Int(numberOfPoints * 2.0)
+        let theta = .pi / numberOfPoints
+
+        let circumscribedRadius = center.x
+        let outerRadius = circumscribedRadius * 1.039
+        let excessRadius = outerRadius - circumscribedRadius
+        let innerRadius = CGFloat(outerRadius * 0.382)
+
+        let leftEdgePointX = (center.x + cos(4.0 * theta) * outerRadius) + excessRadius
+        let horizontalOffset = leftEdgePointX / 2.0
+
+        // Apply a slight horizontal offset so the star appears to be more
+        // centered visually
+        let offsetCenter = CGPoint(x: center.x - horizontalOffset, y: center.y)
+
+        // Alternate between the outer and inner radii while moving evenly along the
+        // circumference of the circle, connecting each point with a line segment
+        for i in 0..<numberOfLineSegments {
+            let radius = i % 2 == 0 ? outerRadius : innerRadius
+
+            let pointX = offsetCenter.x + cos(CGFloat(i) * theta) * radius
+            let pointY = offsetCenter.y + sin(CGFloat(i) * theta) * radius
+            let point = CGPoint(x: pointX, y: pointY)
+
+            if i == 0 {
+                starPath.move(to: point)
+            } else {
+                starPath.addLine(to: point)
+            }
+        }
+
+        starPath.close()
+
+        // Rotate the path so the star points up as expected
+        var pathTransform  = CGAffineTransform.identity
+        pathTransform = pathTransform.translatedBy(x: center.x, y: center.y)
+        pathTransform = pathTransform.rotated(by: CGFloat(-.pi / 2.0))
+        pathTransform = pathTransform.translatedBy(x: -center.x, y: -center.y)
+
+        starPath.apply(pathTransform)
         
-        return layer
-    }()
-
+        return starPath
+    }
+    
 }
-
--- CoreAnimationSharingSession/TransformLayerViewController.swift
+++ CoreAnimationSharingSession/TransformLayerViewController.swift
@@ -15,19 +15,13 @@ class TransformLayerViewController: UIViewController {
         return gesture
     }()
     
-    let transformView: UIView = {
-        let view = UIView()
-        view.frame = CGRect(x: 50, y: 100, width: 200, height: 200)
-        return view
-    }()
+    let cubeWidth: CGFloat = 200
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
-        view.addSubview(transformView)
-        transformView.layer.addSublayer(transformLayer)
-        transformView.addGestureRecognizer(panGesture)
+        view.layer.addSublayer(transformLayer)
+        view.addGestureRecognizer(panGesture)
     }
     
     override func viewDidLayoutSubviews() {
@@ -35,11 +29,11 @@ class TransformLayerViewController: UIViewController {
     }
     
     @objc func handlePanGesture(_ recognizer: UIPanGestureRecognizer) {
-        let translation = recognizer.translation(in: self.transformView)
+        let translation = recognizer.translation(in: self.view)
         var transform = CATransform3DIdentity
         
-        transform = CATransform3DRotate(transform, 1.0/200 * translation.x, 0, 1, 0)
-        transform = CATransform3DRotate(transform, -(1.0/200) * translation.y, 1, 0, 0)
+        transform = CATransform3DRotate(transform, 1.0 / cubeWidth * translation.x, 0, 1, 0)
+        transform = CATransform3DRotate(transform, -(1.0 / cubeWidth) * translation.y, 1, 0, 0)
         transformLayer.sublayerTransform = transform
     }
     
@@ -47,40 +41,40 @@ class TransformLayerViewController: UIViewController {
     lazy var transformLayer: CATransformLayer = {
         let layer = CATransformLayer()
         
+        let translationValue = cubeWidth / 2
         // create the front face
-        let transform1 = CATransform3DMakeTranslation(0, 0, 100)
+        let transform1 = CATransform3DMakeTranslation(0, 0, translationValue)
         layer.addSublayer(createSublayer(transform: transform1, color: .blue))
         
         // create the right-hand face
-        var transform2 = CATransform3DMakeTranslation(100, 0, 0)
+        var transform2 = CATransform3DMakeTranslation(translationValue, 0, 0)
         transform2 = CATransform3DRotate(transform2, CGFloat.pi / 2, 0, 1, 0)
         layer.addSublayer(createSublayer(transform: transform2, color: .green))
        
         // create the top face
-        var transform3 = CATransform3DMakeTranslation(0, -100, 0)
+        var transform3 = CATransform3DMakeTranslation(0, -(translationValue), 0)
         transform3 = CATransform3DRotate(transform3, CGFloat.pi / 2, 1, 0, 0)
         layer.addSublayer(createSublayer(transform: transform3, color: .black))
         
         // create the bottom face
-        var transform4 = CATransform3DMakeTranslation(0, 100, 0)
+        var transform4 = CATransform3DMakeTranslation(0, translationValue, 0)
         transform4 = CATransform3DRotate(transform4, -(CGFloat.pi / 2), 1, 0, 0)
         layer.addSublayer(createSublayer(transform: transform4, color: .cyan))
         
         // create the left-hand face
-        var transform5 = CATransform3DMakeTranslation(-100, 0, 0)
+        var transform5 = CATransform3DMakeTranslation(-(translationValue), 0, 0)
         transform5 = CATransform3DRotate(transform5, -(CGFloat.pi / 2), 0, 1, 0)
         layer.addSublayer(createSublayer(transform: transform5, color: .brown))
         
         // create the back face
-        var transform6 = CATransform3DMakeTranslation(0, 0, -100)
+        var transform6 = CATransform3DMakeTranslation(0, 0, -(translationValue))
         transform6 = CATransform3DRotate(transform6, CGFloat.pi, 0, 1, 0)
         layer.addSublayer(createSublayer(transform: transform6, color: .magenta))
         
-        layer.position = transformView.center
+        layer.position = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
         
         return layer
     }()
-    
     
     let tiledLayer: CATiledLayer = {
         let layer = CATiledLayer()
@@ -100,10 +94,9 @@ class TransformLayerViewController: UIViewController {
     
     func createSublayer(transform: CATransform3D, color: UIColor) -> CALayer {
         let layer = CALayer()
-        let size:CGFloat = 200
-        let center = -(size / 2)
+        let center = -(cubeWidth / 2)
         
-        layer.frame = CGRect(x: center, y: center, width: size, height: size)
+        layer.frame = CGRect(x: center, y: center, width: cubeWidth, height: cubeWidth)
         layer.transform = transform
         layer.borderWidth = 1
         layer.backgroundColor = color.cgColor
